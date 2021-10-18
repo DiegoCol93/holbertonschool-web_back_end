915a8ccc075273bc147bb8d44c9ee42d6794d32a
“DiegoCol93” <dfelo93@gmail.com>
2021-10-18 14:34:30 -0500

    |- REMOVED -| files for 0x00

--- 0-add.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-""" Module for storing the add() method definition. """
-
-def add(a: float, b: float) -> float:
-    """ Adds two float numbers a and b and returns the result."""
-    return(a + b)
