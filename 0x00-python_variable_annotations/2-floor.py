012b19818f9e1c526d241a0dab106cd2f9b1a546
“DiegoCol93” <dfelo93@gmail.com>
2021-10-18 15:12:16 -0500

    |- ADDED -| 2. Basic annotations - floor.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module for storing the floor() method definition. """
+from math import floor as f
+
+
+def floor(n: float) -> int:
+    """ Returns the closest minimun integer to the given float n. """
+    return(f(n))
