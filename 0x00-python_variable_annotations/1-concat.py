f2d830c7f09e182fd772249136b5d8564b7de854
“DiegoCol93” <dfelo93@gmail.com>
2021-10-18 15:03:39 -0500

    |- ADDED -| 1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Module for storing the concat() method definition. """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Method for concatenating 2 strings and returning the result. """
+    return(str1 + str2)
