ae6e1372f67ef0ac2d380c9f542fc6721ad82fea
DiegoCol93 <dfelo93@gmail.com>
2021-10-18 23:08:49 -0500

    |- ADDED -| 2nd attempt.

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 """ Module to store the definition of the method to be annotated. """
-from typing import Tuple, List
+from typing import Tuple, List, Any, Union
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """ Given method to annotate and check with mypy """
-    zoomed_in: Tuple = [
+    zoomed_in: Tuple = tuple([
         item for item in lst
-        for i in range(factor)
-    ]
-    return zoomed_in
+        for i in range(int(factor))
+    ])
+    return list(zoomed_in)
 
 
-array = [12, 72, 91]
+# array: Tuple = [12, 72, 91]
 
-zoom_2x = zoom_array(array)
+# zoom_2x = zoom_array(array)
+# print(zoom_2x)
 
-zoom_3x = zoom_array(array, 3.0)
+# zoom_3x = zoom_array(array, 3.0)
+# print(zoom_3x)
