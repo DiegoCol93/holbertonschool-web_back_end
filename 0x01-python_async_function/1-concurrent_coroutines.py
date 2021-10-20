2e2d184289d92ad3e2007b573927730cceefea10
DiegoCol93 <dfelo93@gmail.com>
2021-10-19 23:59:08 -0500

    |- ADDED -| 1. Execute multiple coroutines at the same time with async.

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,22 +1,45 @@
 #!/usr/bin/env python3
-""" Module for storing a basic asyncio sample. """
+"""module"""
+
 import asyncio
-from random import uniform
+import random
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-from typing import List
-
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ Creates n instances of wait_random with the specified max_delay. """
-    queue, delays = [], []
-    while n > 0:
+    """fnunctio 1"""
+    queue, array = [], []
+    for _ in range(n):
         queue.append(wait_random(max_delay))
-        n -= 1
 
-    for delay in asyncio.as_completed(queue):
-        result = await delay
-        delays.append(result)
+    for q in asyncio.as_completed(queue):
+        result = await q
+        array.append(result)
 
-    return delays
+    return array
+
+
+
+# """ Module for storing a basic asyncio sample. """
+# import asyncio
+# from random import uniform
+
+# wait_random = __import__('0-basic_async_syntax').wait_random
+
+# from typing import List
+
+
+# async def wait_n(n: int, max_delay: int) -> List[float]:
+#     """ Creates n instances of wait_random with the specified max_delay. """
+#     queue, delays = [], []
+#     while n > 0:
+#         queue.append(wait_random(max_delay))
+#         n -= 1
+
+#     for delay in asyncio.as_completed(queue):
+#         result = await delay
+#         delays.append(result)
+
+#     return delays
