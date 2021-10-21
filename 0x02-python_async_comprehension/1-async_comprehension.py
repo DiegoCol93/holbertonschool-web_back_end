a3631a408a9cee61c2558f66ce577d9a60076a11
DiegoCol93 <dfelo93@gmail.com>
2021-10-20 23:34:05 -0500

    |- ADDED -| Annotation for return.

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
+from typing import List
 
-import asyncio
-from random import uniform
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """ Uses the async_generator with an async comprehension. """
     result = [random_number async for random_number in async_generator()]
     return(result)
