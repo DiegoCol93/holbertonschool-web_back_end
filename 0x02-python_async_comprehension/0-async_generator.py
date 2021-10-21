1ae2df29202de3d3f311d9212f70ad8231a7fb2e
DiegoCol93 <dfelo93@gmail.com>
2021-10-20 22:16:49 -0500

    |- ADDED -| Annotation for return

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ Module for storing the async_generator coroutine. """
 import asyncio
 from random import uniform
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ Waits for a second and yields a random number. """
     for _ in range(10):
         await asyncio.sleep(1)
