107439e217a3740ee8af9d4d115fe4e692506d2f
“DiegoCol93” <dfelo93@gmail.com>
2021-10-30 13:54:14 -0500

    |- ADDED -| 0. Regex-ing.

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """ 0. Regex-ing module. """
-
 import re
 from typing import List
 
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """ Returns an obfiscated message given the fields. """
-    
+    """ Returns an obfuscated message given the fields. """
+    for field in fields:
+        message = re.sub(r'({}=)(.+?){}'.format(field, separator),
+                         r'\1{}{}'.format(redaction, separator), message)
+    return(message)
