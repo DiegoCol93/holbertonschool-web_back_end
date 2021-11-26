df38bbc329144908bc63eba1a3554fa58945030c
DiegoCol93 <dfelo93@gmail.com>
2021-11-25 23:16:00 -0500

    |- ADDED -| Annotation.

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -5,6 +5,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def root():
+def root() -> render_template:
     """ Root route of the application. """
-    return render_template("0-index.html", title="Welcome to Holberton")
+    return render_template("0-index.html")
