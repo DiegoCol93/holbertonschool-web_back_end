f098d972f37ee26e365aca45a8b0f138174e283f
DiegoCol93 <dfelo93@gmail.com>
2021-11-25 23:39:53 -0500

    |- FIXED -| Flask instantiation.

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -2,11 +2,11 @@
 """ Basic Flask application. """
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
+
 app.url_map.strict_slashes = False
 
-
-@app.route('/', methods["GET"])
-def root() -> str:
+@app.route('/')
+def root() -> render_template:
     """ Root route of the application. """
     return render_template("0-index.html")
