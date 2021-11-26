5857c81ca6a9a993d737845e10bb0a06335bed72
DiegoCol93 <dfelo93@gmail.com>
2021-11-25 23:36:39 -0500

    |- ADDED -| No strict slashes.

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-@app.route('/')
+@app.route('/', methods["GET"])
 def root() -> render_template:
     """ Root route of the application. """
     return render_template("0-index.html")
