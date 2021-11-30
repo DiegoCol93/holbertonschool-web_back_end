b277446b04c563282eae80eaa0712fbab1795ba9
DiegoCol93 <dfelo93@gmail.com>
2021-11-30 14:26:30 -0500

    |- ADDED -| 4. Retrieving lists.

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -7,7 +7,7 @@ import redis
 
 
 def count_calls(method: Callable) -> Callable:
-    """ Decorator method to count calls to the Cache calss methods. """
+    """ Decorator method to count calls of the Cache class methods. """
     @wraps(method)
     def counter(self, data):
         """
@@ -25,16 +25,27 @@ def call_history(method: Callable) -> Callable:
     def history(self, data):
         """ Stores history of inputs and outputs for the given method. """
         self._redis.rpush(method.__qualname__ + ":inputs",
-                          str("('" + data + "',)"))
+                          "('" + str(data) + "',)")
         out = method(self, data)
         self._redis.rpush(method.__qualname__ + ":outputs", out)
         return out
     return history
 
 
+def replay(method):
+    """ Displays the history of a given method. """
+    name = method.__qualname__
+    cls = method.__self__
+    print(f"{name} was called {cls.get_int(name)} times:")
+    inputs = cls._redis.lrange(f"{name}:inputs", 0, -1)
+    outputs = cls._redis.lrange(f"{name}:outputs", 0, -1)
+    calls = list(zip(inputs, outputs))
+    for call in calls:
+        print(f"{name}(*{str(call[0])[2:-1]}) -> {str(call[1])[2:-1]}")
+
+
 class Cache:
     """ Basic cache redis class. """
-
     def __init__(self):
         """ Constructor method. """
         self._redis = redis.Redis()
@@ -59,6 +70,6 @@ class Cache:
         """ Custom get str method. """
         return(self.get(key, str))
 
-    def get_int(self):
+    def get_int(self, key: str):
         """ Custom get int method. """
         return(self.get(key, int))
