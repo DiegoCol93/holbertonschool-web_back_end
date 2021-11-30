822d455d2ad0d5aa771b81b3252cab901bb1b3d1
DiegoCol93 <dfelo93@gmail.com>
2021-11-30 14:52:11 -0500

    |- ADDED -| 5. Implementing an expiring web cache and tracker.

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 """ 5. Implementing an expiring web cache and tracker module. """
 from requests import get as GET
+from typing import Callable
+from functools import wraps
 import redis
 
+def set_page(method: Callable) -> Callable:
+    """ Decorator method to store the count of hits to a url. """
+    @wraps(method)
+    def cache_in(url):
+        """ Inner decorator method. """
+        text = method(url)
+        cache = redis.Redis()
+        cache.setex(f"count:{url}", 10, text)
+    return cache_in
 
+
+@set_page
 def get_page(url: str) -> str:
     """ Obtains the HTML content of a URL and returns it. """
-    cache = redis.Redis()
     r = GET(url)
-    cache.setex(f"count:{url}", 10, r.text)
     return r.text
+
+get_page("http://slowwly.robertomurray.co.uk")
