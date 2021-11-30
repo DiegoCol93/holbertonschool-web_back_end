bd496a0c23761e78b40f0007c5a7254303116ae9
DiegoCol93 <dfelo93@gmail.com>
2021-11-30 15:01:51 -0500

    |- ADDED -| increment call before setex.

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -5,14 +5,15 @@ from typing import Callable
 from functools import wraps
 import redis
 
+cache = redis.Redis()
 
 def set_page(method: Callable) -> Callable:
     """ Decorator method to store the count of hits to a url. """
     @wraps(method)
     def cache_in(url):
         """ Inner decorator method. """
+        cache.incr(f"count:{url}", 1)
         text = method(url)
-        cache = redis.Redis()
         cache.setex(f"count:{url}", 10, text)
     return cache_in
 
